6c39b5bbd65c37936cfbd5e9267162f026b8d5d8
Scoowy <gahonajuanjo@gmail.com>
2018-05-31 10:09:06 -0500

    Ädd Proyecto Arreglos

---
+++ Arreglos/src/arreglos/Arreglos.java
@@ -0,0 +1,16 @@
+package arreglos;
+
+/**
+ *
+ * @author Juan Gahona - Scoowy
+ */
+public class Arreglos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
---
+++ Arreglos/src/arreglos/PrimerArreglo.java
@@ -0,0 +1,17 @@
+package arreglos;
+
+/**
+ *
+ * @author Juan Gahona - Scoowy
+ */
+public class PrimerArreglo {
+
+    public static void main(String[] args) {
+        int[] c = new int[4];
+        c[0] = 10;
+        System.out.println(c.length);
+        for (int i = 0; i < c.length; i++) {
+            System.out.println(c[i]);
+        }
+    }
+}
